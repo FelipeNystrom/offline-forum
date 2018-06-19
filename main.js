3abcd988378828d394e54fc3d4796118a71ff6c1
FelipeNystrom <andre.felipe.nystrom@gmail.com>
2018-06-19 22:37:43 +0200

    add css and button functionality

---
+++ main.js
@@ -0,0 +1,6 @@
+let showSection = document.querySelector('#showInputSection');
+let inputSection = document.querySelector('#inputSection');
+
+showSection.addEventListener('click', e => {
+  inputSection.style.display = 'block';
+});
