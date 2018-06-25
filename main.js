e7f477fd03a49d91fc79fe52d75fc6badaa08c7b
FelipeNystrom <andre.felipe.nystrom@gmail.com>
2018-06-25 14:20:33 +0200

    add dummy data

--- main.js
+++ main.js
@@ -4,7 +4,24 @@ const form = document.querySelector('#form');
 const deckOfPosts = document.querySelector('#deckOfPosts');
 
 // Data variables
-let posts = [];
+let posts = [
+  {
+    id: 0,
+    title: 'Hello world',
+    text:
+      'Lorem Ipsum är en utfyllnadstext från tryck- och förlagsindustrin. Lorem ipsum har varit standard ända sedan 1500-talet, när en okänd boksättare tog att antal bokstäver och blandade dem för att göra ett provexemplar av en bok. Lorem ipsum har inte bara överlevt fem århundraden, utan även övergången till elektronisk typografi utan större förändringar. Det blev allmänt känt på 1960-talet i samband med lanseringen av Letraset-ark med avsnitt av Lorem Ipsum, och senare med mjukvaror som Aldus PageMaker.',
+    author: 'John Doe',
+    comments: [
+      {
+        id: 0,
+        title: 'This is a comment',
+        text:
+          'Det är ett välkänt faktum att läsare distraheras av läsbar text på en sida när man skall studera layouten. Poängen med Lorem Ipsum är att det ger ett normalt ordflöde, till skillnad från "Text här, Text här", och ger intryck av att vara läsbar text. Många publiseringprogram och webbutvecklare använder Lorem Ipsum som test-text, och en sökning efter "Lorem Ipsum" avslöjar många webbsidor under uteckling. Olika versioner har dykt upp under åren, ibland av olyckshändelse, ibland med flit (mer eller mindre humoristiska).',
+        author: 'Jane Doe'
+      }
+    ]
+  }
+];
 
 let _postId = 0;
 
